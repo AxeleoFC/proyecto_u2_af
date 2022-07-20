f6cfce59e104b23b5cf1981eccc128e9869e5514
AxeleoFC <axeleo2000@outlook.com>
2022-07-19 20:08:40 -0500

    Taller 24

--- src/main/java/com/uce/edu/demo/ProyectoU2AfApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2AfApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu.demo;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,7 +10,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
 import com.uce.edu.demo.repository.modelo.PersonaContadorGenero;
+import com.uce.edu.demo.service.ICiudadanoService;
 import com.uce.edu.demo.tarea17_18_19_20.repository.modelo.EstudianteQueryPorCarrera;
 import com.uce.edu.demo.tarea17_18_19_20.repository.modelo.EstudianteQuerySencillo;
 import com.uce.edu.demo.tarea17_18_19_20.service.IEstudianteJPAQueryService;
@@ -23,8 +27,11 @@ public class ProyectoU2AfApplication implements CommandLineRunner{
 	private static final Logger LOG = LogManager.getLogger(ProyectoU2AfApplication.class.getName());
 
 	@Autowired
-	private IEstudianteJPAQueryService estudianteJPACService;
+	private ICiudadanoService ciudadanoService;
 
+	//@Autowired
+	//private IEmpleadoService empleadoService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2AfApplication.class, args);
 	}
@@ -33,17 +40,19 @@ public class ProyectoU2AfApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		//Busqueda sencilla
-		List<EstudianteQuerySencillo> listaEstudiante = this.estudianteJPACService.buscarPorCarreraSencillo("Computacion");
-		listaEstudiante.stream().forEach(estudiante -> {
-			LOG.info("Estudiante sencillo por carrera: " + estudiante);
-		});
+		Ciudadano ciu1=new Ciudadano();
+		ciu1.setNombre("Jose");
+		ciu1.setApellido("Vinueza");
 		
-		//Busqueda sencilla con agrupamiento
-		String facultad="Ingenieria en ciencias aplicadas.";
-		List<EstudianteQueryPorCarrera> listaEstudiante1 = this.estudianteJPACService.consultarEstudiantesPorCarrera(facultad);
-		listaEstudiante1.stream().forEach(estudiante -> {
-			LOG.info("Estudiantes por carrera de la facultad de "+facultad+" : " + estudiante);
-		});
+		Empleado empl1=new Empleado();
+		empl1.setCodigoIESS("143145");
+		empl1.setSalario(new BigDecimal(150));
+		empl1.setCiudadano(ciu1);
+		
+		ciu1.setEmpleado(empl1);
+		
+		this.ciudadanoService.ingresarCiudadano(ciu1);
+		
+		
 	}
 }
